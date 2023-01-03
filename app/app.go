22fe6f4e96ddb52159269d969beac687a5e1cce7
Vitor Rocha <deus.1928@gmail.com>
2023-01-03 18:07:46 -0400

    feat: command to search server name

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "CLI"
 	app.Usage = "Search IPs and Servername in Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IPs in Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IPs in Internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "sn",
+			Usage:  "Search servername in Internet",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 	return app
@@ -40,5 +48,18 @@ func searchIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServer(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, s := range servers {
+		fmt.Println(s.Host)
 	}
 }
